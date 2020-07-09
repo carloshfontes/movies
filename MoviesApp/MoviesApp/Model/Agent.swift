1a79384fc57e257a7c52a234cadad12847112a75
Carlos <carloshenriquenlb@gmail.com>
2020-07-09 10:31:29 -0300

    Add movie and TmdbApi model

--- MoviesApp/MoviesApp/Model/Agent.swift
+++ MoviesApp/MoviesApp/Model/Agent.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 Carlos Fontes. All rights reserved.
 //
 
+import Combine
 import Foundation
+import UIKit
+
+
+struct Agent {
+    
+    struct Response<T>{
+        let value: T
+        let response: URLResponse
+    }
+    
+    func runApi(_ request: URLRequest) -> AnyPublisher<[Movie], Error> {
+        return URLSession.shared.dataTaskPublisher(for: request)
+        .tryMap { element -> Data in
+                guard let httpResponse = element.response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return element.data
+            }
+        .decode(type: Results.self, decoder: JSONDecoder())
+        .tryMap {results -> [Movie] in
+            return results.results
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func imageLoader(_ request: URLRequest) -> AnyPublisher<UIImage, Error> {
+        return URLSession.shared.dataTaskPublisher(for: request)
+        .tryMap { element -> Data in
+                guard let httpResponse = element.response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return element.data
+            }
+        .tryMap {data in
+            guard let image = UIImage(data: data) else {
+                throw MovieError.statusCode
+            }
+            return image
+        }
+        .eraseToAnyPublisher()
+    }
+}
--- MoviesApp/MoviesApp/Model/Movie.swift
+++ MoviesApp/MoviesApp/Model/Movie.swift
@@ -16,9 +16,35 @@ struct Movie: Codable {
     let popularity: Double
     let release_date: String
     let vote_average: Double
+    
+    var releaseDate: Int? {
+        let formatter = Movie.dateFormatter.date(from: release_date)
+        return Calendar.current.component(.year, from: formatter ?? Date())
+    }
+    
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd"
+        return formatter
+    }()
+    
+    public var posterURL: URL {
+        return URL(string: "https://image.tmdb.org/t/p/w500\(poster_path)")!
+    }
 }
 
 
 struct Results: Codable{
     let results: [Movie]
+}
+
+
+enum MovieError: Error {
+    case statusCode
+    case decoding
+    case other(Error)
+    
+    static func map(_ error: Error) -> MovieError {
+        return (error as? MovieError) ?? .other(error)
+    }
 }
